7f7040c7807fdab2598ce723880d16049a80187c
rsaat <ricardo@presys.com.br>
2021-02-12 00:19:14 -0300

    Projeto Inicial Hello world Console

---
+++ HelloTorizon/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloTorizon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var i=5;
+            Console.WriteLine("Hello World2!");
+        }
+    }
+}
